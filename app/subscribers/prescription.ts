649d66279b4acac32720b9ff36d74868411b356c
jacinta254 <jecihjoy@gmail.com>
2021-10-25 14:19:19 +0300

    Create amrs drug orders enhancements

--- app/subscribers/prescription.ts
+++ app/subscribers/prescription.ts
@@ -4,6 +4,8 @@ import ADTRESTClient from "../loaders/ADT-rest-client";
 import RegimenLoader from "../loaders/regimen-mapper";
 import { loadProviderData } from "../models/patient";
 import PrescriptionService from "../services/prescription";
+import * as _ from "lodash";
+import orderPayload from "../poc_sample_payload";
 
 @EventSubscriber()
 export default class PrescriptionSubscriber {
@@ -105,31 +107,23 @@ export default class PrescriptionSubscriber {
       );
   }
   @On("createAMRSOrder")
-  public async onCreateAMRSOrder({ patient, encounter }: any) {
-    const prescriptionService = new PrescriptionService();
-    // Call AMRS orders/Encounter endpoint to create the order
-    const data = new ADTRESTClient("amrs");
-    let patients: Patient.Patient = patient[0];
-    let enc: any = encounter[0];
-    console.log(encounter[0].location_uuid, patients);
-    const payload: IOrder.DrugOrders = {
-      encounterDatetime: new Date(patients.encounter_datetime).toISOString(),
-      patient: enc.patient_uuid,
-      location: enc.location_uuid,
-      encounterType: enc.encounter_type_uuid,
-      encounterProviders: [
-        {
-          provider: enc.provider_uuid,
-          encounterRole: enc.encounter_role,
-        },
-      ],
-      orders: [
-        {
-          // Hardcoded concept for medication order since this is currently not entered by clinicians
-          concept: "a890c3aa-1350-11df-a1f1-0026b9348838",
-          careSetting: "6f0c9a92-6f24-11e3-af88-005056821db0",
+  public async onCreateAMRSOrder(x: any) {
+    let savedOrders: any[] = [];
+    if (orderPayload.drugOrders !== undefined) {
+      const data = new ADTRESTClient("amrs");
+      const lastItem = orderPayload.drugOrders.slice(-1)[0];
+      orderPayload.drugOrders.forEach((curOrder: any) => {
+        /** Construct AMRS order payload */
+        let payload = {
           type: "drugorder",
-          orderer: "f5c7ce7d-e038-45c6-afd8-37c4e541429d",
+          action: "new",
+          urgency: "ROUTINE",
+          dateActivated: new Date(),
+          careSetting: "OUTPATIENT",
+          encounter: orderPayload.encounter,
+          patient: orderPayload.patient,
+          concept: curOrder.uuid,
+          orderer: orderPayload.orderer[0].provider.uuid,
           dose: 20,
           doseUnits: "a8a07f8e-1350-11df-a1f1-0026b9348838",
           route: "db0c5937-3874-4eae-9566-9a645ad7ac65",
@@ -137,51 +131,51 @@ export default class PrescriptionSubscriber {
           quantity: 10,
           numRefills: 1,
           quantityUnits: "a8a07f8e-1350-11df-a1f1-0026b9348838",
-        },
-      ],
-      obs: [],
-    };
-    if (patients.weight == null || patients.height == null) {
-      return;
+        };
+        data.axios
+          .post("ws/rest/v1/order", payload)
+          .then(async (resp: HTTPResponse) => {
+            savedOrders.push(resp);
+            if (lastItem.uuid === curOrder.uuid) {
+              /** TODO
+               * Initiate events for creating and sending order prescription to ADT,
+               * pass AMRS order details that will be use to construct ADT prescription payload.
+               */
+              console.log("SAVED ORDERS ** ", savedOrders);
+            }
+          })
+          .catch(
+            (error: {
+              response: { data: any; status: any; headers: any };
+              request: any;
+              message: any;
+              config: any;
+            }) => {
+              // Error 😨
+              if (error.response) {
+                /*
+                 * The request was made and the server responded with a
+                 * status code that falls out of the range of 2xx
+                 */
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                /*
+                 * The request was made but no response was received, `error.request`
+                 * is an instance of XMLHttpRequest in the browser and an instance
+                 * of http.ClientRequest in Node.js
+                 */
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request and triggered an Error
+                console.log("Error", error.message);
+              }
+              console.log(error.config);
+            }
+          );
+      });
     }
-    data.axios
-      .post("ws/rest/v1/encounter", payload)
-      .then(async (resp: HTTPResponse) => {
-        console.log(resp.orders);
-        if (resp.uuid) {
-          await this.fetchOrderNumber(resp.orders[0].uuid, data, patient);
-        }
-      })
-      .catch(
-        (error: {
-          response: { data: any; status: any; headers: any };
-          request: any;
-          message: any;
-          config: any;
-        }) => {
-          // Error 😨
-          if (error.response) {
-            /*
-             * The request was made and the server responded with a
-             * status code that falls out of the range of 2xx
-             */
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            /*
-             * The request was made but no response was received, `error.request`
-             * is an instance of XMLHttpRequest in the browser and an instance
-             * of http.ClientRequest in Node.js
-             */
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request and triggered an Error
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        }
-      );
   }
   public async fetchOrderNumber(
     body: any,

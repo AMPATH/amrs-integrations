9cb95d56bf09ea436890fa546c3c96968597917d
Marcelkibor <kibormarcel90@gmail.com>
2023-04-14 12:32:49 +0300

    Archive and Void CSV's

--- packages/eid/app/services/csv_upload.ts
+++ packages/eid/app/services/csv_upload.ts
@@ -2,6 +2,7 @@ import Validators from "../helpers/validators";
 import path from "path";
 import * as fs from "fs";
 import csv from "csv-parser";
+import cron from 'node-cron';
 import GetPatient from "../helpers/dbConnect";
 export default class UploadSaveAndArchiveCSV {
   public async uploadFile(file: any, username: string, file_type: string, total_records: number) {
@@ -47,7 +48,7 @@ export default class UploadSaveAndArchiveCSV {
         });
       }
       // upload the file
-      const res = await this.handleFileUpload(file, uploadPath);
+      const res = await this.handleFileUpload(file, uploadPath, username);
 
       return res;
     } catch (error) {
@@ -56,7 +57,7 @@ export default class UploadSaveAndArchiveCSV {
     return { error: "Failed to upload file", status: 'error' };
   }
 
-  handleFileUpload = async (file: any, uploadPath: string) => {
+  handleFileUpload = async (file: any, uploadPath: string, username:string) => {
     // const options = { headers: true, quoteColumns: true };
     const stream = fs.createWriteStream(uploadPath);
     file.pipe(stream);
@@ -64,7 +65,106 @@ export default class UploadSaveAndArchiveCSV {
     return new Promise((resolve, reject) => {
       stream
         .on("error", (err) => console.error(err))
-        .on("finish", () => resolve({ message: "Upload successfully!" , status: 'success'}));
+        .on("finish", () => resolve((this.updateUploadLogs(file.hapi.filename,username),{ message: "Upload successfully!" , status: 'success'})));
     });
   };
+  //log file for new csv files in the upload subfolders
+  updateUploadLogs = async (file:any,username:string)=>{
+    const logFilePath = path.join(
+      path.dirname(__dirname),
+      `../app/uploads/${username}/logs.txt`
+    );
+    fs.readFile(logFilePath, "utf-8", function(err, data) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // Log file does not exist, create new array
+          data = '[]';
+        } else {
+          throw err;
+        }
+      }
+      const logs = JSON.parse(data);
+      const fileLog = {
+        file_name: file,
+        logged_user: username,
+        uploadTime: new Date().toISOString()
+      };
+      logs.push(fileLog);
+      fs.writeFile(logFilePath, JSON.stringify(logs), (err) => {
+        if (err) throw err;
+        return fileLog
+      });  
+    });
+  }
+//Archive csv uploads when server starts & weekly if server is up
+handleArchive = async()=>{
+  const uploadsPath = path.join(path.dirname(__dirname), '../app/uploads/');
+  const uploadSubFolders = await Promise.all(await this.getUploadSubFolders(uploadsPath));
+  this.checkCsvUploadTime(uploadSubFolders)
+  const archive = cron.schedule('0 0 */7 * *', async () => {
+    this.checkCsvUploadTime(uploadSubFolders);
+  });
+  archive.start();
 }
+//check the upload time of a csv in the logs file.
+checkCsvUploadTime = async (uploadSubFolders: any) => {
+  for (const subFolder of uploadSubFolders) {
+    const logs = path.join(subFolder, `logs.txt`);
+      const logPath = fs.statSync(logs);
+      if (logPath.isFile()) {
+        const data = fs.readFileSync(logs, `utf-8`);
+        const fileLogs = JSON.parse(data);
+        for (const log of fileLogs) {
+          const now = new Date();
+          const csvPath = path.join(subFolder, `${log.file_name}`);
+          const uploadTime = new Date(log.uploadTime);
+          const one_week_in_ms = 604800000;
+          const timeDifference = now.getTime() - uploadTime.getTime();
+          // if upload time > 1 week && file exists, delete from folder.
+          if (fs.existsSync(csvPath) && fs.statSync(csvPath).isFile() && timeDifference > one_week_in_ms) {
+           this.deleteCsvFile(csvPath);
+          } 
+        }
+      }
+  }
+};
+//delete csv file from upload subfolders
+deleteCsvFile = async(csvPath:string)  =>{
+return new Promise<void>((resolve, reject) => {
+  fs.unlink(csvPath, async (err) => {
+    if (err) {
+      reject();
+    } else {
+      let csvFile = csvPath.split("/").pop();
+      await this.voidUploads(csvFile)
+      resolve();
+    }
+  });
+});
+}
+voidUploads = async(csvFile:any)=>{
+  let uploadPayload = {
+    file_name: csvFile,
+  }
+  try{
+    const query = new GetPatient();
+    // if csv metadata is in etl, void record
+    let fileExists = await query.checkIfFileExists(uploadPayload);
+    if(fileExists.length>0){
+      let csvfileId = fileExists[0].eid_file_upload_metadata_id; 
+      await query.voidEidCsvMetaData(csvfileId)
+    }
+  }catch(e:any){
+    return e;
+  }
+}
+// get path to existing subfolders in uploads
+getUploadSubFolders = async(dir:string)=>{
+  const subFolders = fs.readdirSync(dir);
+  return subFolders.filter(subFolders => {
+    const filePath = path.join(dir, subFolders);
+    const stat = fs.statSync(filePath);
+    return stat.isDirectory();
+  }).map(subFolders=>path.join(dir,subFolders))
+}}
+new UploadSaveAndArchiveCSV().handleArchive();

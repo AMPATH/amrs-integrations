35dcc6638a3e33a28b485d9ae5d056f478c39cb1
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 <longkhery@gmail.com>
2023-06-22 15:32:59 +0300

    added the ushauri appointment and registration payload skeleton

--- packages/sms-reminders/index.ts
+++ packages/sms-reminders/index.ts
@@ -2,6 +2,7 @@ import { RetrieveAppointments, SendNotifications } from "./app/appointments";
 import config from "@amrs-integrations/core"
 import {setConfiguration} from "redis-smq"
 import { SendSMS, UpdateDelivery } from "./app/sms";
+
 let redisConfig:any =config.redis
 setConfiguration(redisConfig)
 //Run producer and consumer

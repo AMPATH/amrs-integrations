6080fe3079fe8edb20cdc2f542543b960e62d1c2
henrykorir <longkhery@gmail.com>
2023-06-26 23:12:49 +0300

    made the function to be solely for retrieving registrations

--- packages/sms-reminders/helper/get-registration.ts
+++ packages/sms-reminders/helper/get-registration.ts
@@ -14,8 +14,8 @@ import{
 }
 from '../models/registration';
 import example from '../models/example.registration_payload.json'
-import { now } from 'moment';
 import { Patient } from '../models/patient';
+
 
 export const getRegistration = async (param: Patient) => {
     /**
@@ -23,6 +23,7 @@ export const getRegistration = async (param: Patient) => {
      * 2). Populate the payload
      * 3). return the payload for consumption
      */
+ 
     const payload: Registration = example;
     const currentRegistrations: any[] = [payload]; //all registrations pulled by this time
 

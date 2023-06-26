e33780d9c2a858d005fac9f6d22489574ce4a055
henrykorir <longkhery@gmail.com>
2023-06-26 17:23:57 +0300

    Added module to handle sending appointment and registration payloads to openhim

--- packages/sms-reminders/helper/send-to-ushauri.ts
+++ packages/sms-reminders/helper/send-to-ushauri.ts
@@ -1,7 +1,6 @@
 import config from "@amrs-integrations/core";
 import { isSafaricomNumber, retrievePhoneCarrier } from "./get-carrier-prefix";
 import { getRegistration } from "./get-registration";
-import { Patient } from "../models/patient";
 import { getAppointment } from "./get-appointment";
 
 export const sendRegistrationToUshauri = async (params: any) => {
@@ -24,14 +23,15 @@ export const sendRegistrationToUshauri = async (params: any) => {
     console.log(sendToOpenHIM);
 }
 export const sendAppointmentToUshauri =async (params: any) => {
-    let paylod: any = await getAppointment(params.smsParams);
+    let payload: any = await getAppointment(params.smsParams);
     let carrier = retrievePhoneCarrier(params.natnum);
     let isSaf: boolean = isSafaricomNumber(carrier);
 
-    if (isSaf === false)
+    if (isSaf === true)
     {
-
+        //TODO: set the payload CONSENT_FOR_REMINDER to 'N' To make advanta happy
     }
+
     let httpClient = new config.HTTPInterceptor(
         "https://openhimapi.kenyahmis.org/rest/api" || "",
         "test@gmail.com",
@@ -43,9 +43,9 @@ export const sendAppointmentToUshauri =async (params: any) => {
         "/IL/registration/test",
         {
             method: "post",
-            data: paylod,
+            data: payload,
         }
     );
-    
+
     console.log(sendToOpenHIM);
 }

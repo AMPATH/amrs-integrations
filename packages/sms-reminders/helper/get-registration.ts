3bbb30c909c72916c58cb1ff5c7e53f432e2b60f
henrykorir <longkhery@gmail.com>
2023-06-27 10:20:55 +0300

    fix env

--- packages/sms-reminders/helper/get-registration.ts
+++ packages/sms-reminders/helper/get-registration.ts
@@ -16,13 +16,16 @@ from '../models/registration';
 import example from '../models/example.registration_payload.json'
 import { Patient } from '../models/patient';
 
-
 export const getRegistration = async (param: Patient) => {
     /**
-     * 1). Pull patient registration data from DB
-     * 2). Populate the payload
-     * 3). return the payload for consumption
+     * 1). Query the DBs for columns required in the payload
+     * 2). populate the payload
+     * 3). Return the payload
      */
+    //scenarios: If it is a ne client
+    /*
+    a). The client registration payload is build
+    */
  
     const payload: Registration = example;
     const currentRegistrations: any[] = [payload]; //all registrations pulled by this time

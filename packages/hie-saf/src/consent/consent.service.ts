1f27f3a2625dcd661f719633ff9a4cd5170798a5
Felix Maiko <maikofelix89@gmail.com>
2026-05-13 12:05:10 +0300

    (feat) SAF-HIE: Add validate OPT (#97)

--- packages/hie-saf/src/consent/consent.service.ts
+++ packages/hie-saf/src/consent/consent.service.ts
@@ -1,8 +1,14 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HieHttpRequests } from '../hie-http-request/hie-http-requests';
 import { RequestConsentDto } from '../shared/dto/request-consent.dto';
-import { RequestOtpApiResponse } from '../shared/types';
+import {
+  RequestOtpApiResponse,
+  TiberbuRequestOtpApiResponse,
+  TiberbuValidateConsentApiResponse,
+  ValidateConsentApiResponse,
+} from '../shared/types';
+import { ValidateConsentDto } from 'src/shared/dto/validate-consent.dto';
 
 @Injectable()
 export class ConsentService {
@@ -21,7 +27,12 @@ export class ConsentService {
       );
       const data = (await response.json()) as RequestOtpApiResponse;
       if (data.status === 'success') {
-        return data;
+        const tiberbuResp: TiberbuRequestOtpApiResponse = {
+          message: data.message ?? 'OTP sent successfully',
+          sessionId: data.id,
+          maskedPhone: requestConsentDto.phoneNumber,
+        };
+        return tiberbuResp;
       } else if (data.status === 'error') {
         throw new HttpException(
           data.error ?? 'Error ocurred when requesting OTP, please retry again',
@@ -37,6 +48,52 @@ export class ConsentService {
       console.error(error);
       throw new HttpException(
         'Error fetching Data',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  async validateClientConsent(validateConsentDto: ValidateConsentDto) {
+    const baseUrl = this.configService.get<string>('HIE_BASE_URL') ?? '';
+    const clientSearchUrl = `${baseUrl}/hie/api/v1/consent-service/validate-otp`;
+
+    try {
+      const response = await this.hieHttpRequests.sendPostRequest(
+        clientSearchUrl,
+        validateConsentDto,
+      );
+      if ('ok' in response && response['ok'] === false) {
+        throw new HttpException(
+          'Error ocurred when validating OTP, please retry again',
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
+      const data = (await response.json()) as ValidateConsentApiResponse;
+      const errorMsg = (
+        data['message']
+          ? data['message']
+          : 'Error ocurred when validating OTP, please retry again'
+      ) as string;
+      if (data['status'] === 'success') {
+        const tiberbuResponse: TiberbuValidateConsentApiResponse = {
+          data: {
+            identification_type: '',
+            identification_number: '',
+            status: 'valid',
+          },
+        };
+        return tiberbuResponse;
+      } else if (data['status'] === 'error') {
+        throw new HttpException(errorMsg, HttpStatus.INTERNAL_SERVER_ERROR);
+      } else {
+        throw new HttpException(
+          'Error ocurred when requesting OTP, please retry again',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    } catch (error: any) {
+      Logger.error(error);
+      throw new HttpException(
+        (error.message as string) ?? 'Error Validating OTP',
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }

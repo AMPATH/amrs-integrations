c89bfe789a4b285c12a3081ab9f564565963b585
Felix Maiko <maikofelix89@gmail.com>
2026-06-05 13:22:24 +0300

    (feat) Add Patient Benefits utilization module

--- packages/hie-saf/src/claims/claims.module.ts
+++ packages/hie-saf/src/claims/claims.module.ts
@@ -4,10 +4,20 @@ import { SubBenefitsController } from './claims-eligibility/sub-benefits/sub-ben
 import { SubBenefitsService } from './claims-eligibility/sub-benefits/sub-benefits.service';
 import { InterventionsController } from './claims-eligibility/interventions/interventions.controller';
 import { InterventionsService } from './claims-eligibility/interventions/interventions.service';
+import { BenefitsUtilizationController } from './claims-eligibility/benefit-utilization/benefits-utilization.controller';
+import { BenefitsUtilizationService } from './claims-eligibility/benefit-utilization/benefits-utilization.service';
 
 @Module({
   imports: [HieHttpRequestModule],
-  controllers: [SubBenefitsController, InterventionsController],
-  providers: [SubBenefitsService, InterventionsService],
+  controllers: [
+    SubBenefitsController,
+    InterventionsController,
+    BenefitsUtilizationController,
+  ],
+  providers: [
+    SubBenefitsService,
+    InterventionsService,
+    BenefitsUtilizationService,
+  ],
 })
 export class ClaimsModule {}

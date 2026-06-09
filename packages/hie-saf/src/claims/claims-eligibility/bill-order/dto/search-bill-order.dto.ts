a498f56ea47d9b8b8f790b1a9fec8575d8efff54
Felix Maiko <maikofelix89@gmail.com>
2026-06-09 10:16:38 +0300

    (fix) Get bill order and handle bill order not found exception

--- packages/hie-saf/src/claims/claims-eligibility/bill-order/dto/search-bill-order.dto.ts
+++ packages/hie-saf/src/claims/claims-eligibility/bill-order/dto/search-bill-order.dto.ts
@@ -1,10 +1,4 @@
-import { IsNotEmpty, IsString } from 'class-validator';
 export class SearchBillOrderDto {
-  @IsNotEmpty()
-  @IsString()
   bill_uuid?: string;
-
-  @IsNotEmpty()
-  @IsString()
   order_no?: string;
 }

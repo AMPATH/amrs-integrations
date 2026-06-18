074b94cc9c6c82255b8458f9e7d1c559589976b8
Felix Maiko <maikofelix89@gmail.com>
2026-06-18 16:54:16 +0300

    (feat) Add connection pool size env

--- packages/hie-saf/src/shared/middleware/logger.middleware.ts
+++ packages/hie-saf/src/shared/middleware/logger.middleware.ts
@@ -4,11 +4,9 @@ import { Request, Response, NextFunction } from 'express';
 export function logger(req: Request, res: Response, next: NextFunction) {
   const { ip, method, url } = req;
   const userAgent = req.get('user-agent') || '';
-
   res.on('close', () => {
     const { statusCode } = res;
     const contentLength = res.get('content-length');
-
     Logger.log(
       `${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
     );

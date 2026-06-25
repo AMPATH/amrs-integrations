ed140fa41161c26aff26030331cc5836304c1880
Felix Maiko <maikofelix89@gmail.com>
2026-06-25 20:00:09 +0300

    (feat) Add swagger docs (#122)

--- packages/hie-saf/src/claims/claims-eligibility/interventions/dto/interventions.dto.ts
+++ packages/hie-saf/src/claims/claims-eligibility/interventions/dto/interventions.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class InterventionsDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   patient_id!: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   sub_benefit_code!: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   locationUuid!: string;

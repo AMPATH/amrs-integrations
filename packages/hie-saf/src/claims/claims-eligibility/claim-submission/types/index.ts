7d335e52fcaed5fc2ae58b0415a922d8170ce6a7
Felix Maiko <maikofelix89@gmail.com>
2026-07-21 17:25:15 +0300

    Add inpatient submission

--- packages/hie-saf/src/claims/claims-eligibility/claim-submission/types/index.ts
+++ packages/hie-saf/src/claims/claims-eligibility/claim-submission/types/index.ts
@@ -6,3 +6,13 @@ export type SubmitClaimDto = {
   discharge_reason: string;
   notes: string;
 };
+
+export type SubmitInpatientClaimDto = {
+  consent_token: string;
+  invoice_number: string;
+  otp?: string;
+  discharge_auth_guid?: string;
+  discharge_reason: string;
+  notes: string;
+  discharge_date: string;
+};
